c6ca09d6f70cc6bc60c8522429fc889417d0ccd6
Andrew Nicolalde <andrew.nicolalde@gmail.com>
2019-02-23 15:33:10 +0000

    Finish lesson 3

---
+++ lesson_3/calculating_string_length.asm
@@ -0,0 +1,30 @@
+SECTION .data
+msg db 'Hello brave new world! Here are some more characters. And maybe some more. Does it really matter?', 0ah    ; Longer string this time
+
+SECTION .text
+global _start
+
+_start:
+    mov ebx, msg    ; Move the address of our message into EBX
+    mov eax, ebx    ; Move the contents of EBX (which is the address of the message string) into EAX
+                    ; We are going to move EAX forward by one byte until we reach the end of the string.
+
+nextchar:
+    cmp byte [eax], 0   ; Compare byte pointed to by eax with 0. 0 is an end of string delimiter.
+    jz finished         ; If byte at the address stored in EAX is 0, jump to finished. 
+    inc eax             ; Else, increment the address at EAX by one byte.
+    jmp nextchar        ; Jump back to the point in the code labeled nextchar and compare again. This is a loop.
+
+finished:
+    sub eax, ebx        ; Subtract the address at EBX from EAX. Remember that we have been incrementing EAX, so it will always be larger.
+                        ; When you subtract two memory addresses of the same type from one another, the result is the number of segments
+                        ; between them.
+    mov edx, eax        ; Copy the calculated length of the string from EAX into EDX, as is necessary for sys_write
+    mov ecx, msg        ; Put the address of msg into ecx, as is necessary for sys_write
+    mov ebx, 1          ; Put the file descriptor for STDOUT into ebx, as is necessary for sys_write
+    mov eax, 4          ; Write the opcode for sys_write into EAX
+    int 80h             ; Trigger an interrupt.
+
+    mov ebx, 0  ; Set exit status code to 0
+    mov eax, 1  ; Set opcode for sys_exit
+    int 80h     ; Trigger an interrupt.
---
+++ lesson_3/helloworld_length_problem.asm
@@ -0,0 +1,23 @@
+; If we want the program to exit correctly, we must call sys_exit, which happens to be opcode 1
+
+; Everything is the same as before
+SECTION .data
+msg db 'Hello world, and here is some more text!', 0ah ; assign message varibale with the string that is our message, 0ah is linefeed
+; Because we now have a longer string, and we have not updated the length of the string, only the first 13 bytes will be printed, as before.
+
+SECTION .text
+global _start   ; Apparently this denotes the entrypoint for our program.
+
+_start:
+    mov edx, 13  ; For sys_write, EDX must be loaded with the length in bytes of the message to write.
+    mov ecx, msg    ; For sys_write, ECX is loaded with the memory address of our message string.
+    mov ebx, 1      ; For sys_write, EBX is loaded with the file descriptor for the file we want to write to, in this case STDOUT
+    mov eax, 4      ; After a software interrupt, the kernel will invoke whatever function is denoted by the opcode we load into EAX
+                    ; In this case, the function denoted by opcode 4 is sys_write, though some online sources seem to contradict this.
+    int 80h         ; Call the kernel, telling it to execute the function denoted by the opcode loaded into EAX.
+
+    ; Everything above is the same as in lesson 1, everything below is the code that will make the program exit gracefully.
+
+    mov ebx, 0  ; For sys_exit, the value of ebx is the exit status code.
+    mov eax, 1  ; Load the opcode for sys_exit into EAX, from which the kernel will read.
+    int 80h     ; Recall that int means interrupt, NOT integer.
