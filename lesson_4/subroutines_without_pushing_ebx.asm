accec5ea63818fb2756f8df6c9d9a6e25d289b06
Andrew Nicolalde <andrew.nicolalde@gmail.com>
2019-02-24 22:30:11 +0000

    Finish lesson 4

---
+++ lesson_4/subroutines_without_pushing_ebx.asm
@@ -0,0 +1,37 @@
+; This file wil segfault
+SECTION .data
+msg db 'Hello world! This is a long message which we will try to determine the length of with subroutines. and we arent pushing ebx', 0ah
+
+SECTION .text
+global _start
+
+_start:
+    mov eax, msg    ; Put the address of our message string into eax
+    call strlen     ; Call our function
+
+    mov edx, eax    ; Put the length of the string calculated in strlen into edx, as required by sys_write
+    mov ecx, msg    ; Put the address of the string message into ECX, as required by sys_write
+    mov ebx, 1      ; Put the file descriptor for STDOUT into ebx, as required by sys_write
+    mov eax, 4      ; Put the opcode for sys_write into eax
+    int 80h         ; Trigger an interrupt.
+
+    ; Exit gracefully
+    mov eax, 1      ; Put opcode for sys_exit into eax
+    mov ebx, 0      ; Put exit code 0 into ebx
+    int 80h         ; Trigger an interrupt.
+    
+strlen:             ; Our function declaration
+    mov ebx, eax    ; Copy the address of the message string from eax, where it is stored to ebx so that we can perform our pointer arithmetic.
+
+nextchar:
+    cmp byte [eax], 0   ; Check if the value stored at the address stored in eax is 0
+    jz finished         ; If yes, jump to the "finished" label in the code.
+                        ; jz = Jump if Zero
+                        ; jz jumps to the specified point when the "zero flag" is set.
+                        ; More info on the zero flag: https://en.wikipedia.org/wiki/Zero_flag
+    inc eax             ; Else (if zero flat is NOT set), increment eax by one byte
+    jmp nextchar         ; Jump to the "nextchar" label in the code, effectively a loop.
+
+finished:
+    sub eax, ebx    ; Subtract ebx from eax and store the result in eax.
+    ret             ; Return to the calling function.
